a75547c439ff12d1f80c20341144cb5a7116eaa9
kthong3 <kthongsakounh@gmail.com>
2020-08-28 10:32:14 -0500

    add item view and ability to delete item from todo list

--- MyToDoList/ViewController.swift
+++ MyToDoList/ViewController.swift
@@ -51,6 +51,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.deselectRow(at: indexPath, animated: true)
         
         // Open the screen to see item info and delete
+        let item = data[indexPath.row]
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "view") as? ViewViewController else {
+            return
+        }
+        
+        vc.item = item
+        vc.deletionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = item.item
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func didTapAddButton() {

db6f4ede6984d2a385e4e99c2334d1c2c9b6d700
kthong3 <kthongsakounh@gmail.com>
2020-08-28 09:10:06 -0500

    add table view

--- MyToDoList/ViewController.swift
+++ MyToDoList/ViewController.swift
@@ -5,16 +5,52 @@
 //  Created by Katherine Thongsakounh on 8/28/20.
 //  Copyright © 2020 Katherine Thongsakounh. All rights reserved.
 //
-
+import RealmSwift
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ - show list of current to do list items
+ - enter new to do list items
+ - show previously entered items
+ - delete previously entered items
+ */
 
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    private var data = [ToDoListItem]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
     }
-
+    
+    // Mark: Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Open the screen to see item info and delete
+    }
 
 }
 
